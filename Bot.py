5cbd511c873598e0d3fbfd3b83106d447216de94
obeidanoorhamdan-png <obeidanoorhamdan@gmail.com>
2026-03-16 15:57:29 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -42,9 +42,9 @@ try:
     from fake_useragent import UserAgent
     from faker import Faker
     from colorama import init, Fore, Style, Back
-    import telegram
-    from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-    from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton, Update
+    import telebot  # هذا هو السطر المهم
+    from telebot import types
+    from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 except ImportError as e:
     print(f"❌ خطأ في استيراد المكتبات: {e}")
     print("⚠️ تأكد من تثبيت المكتبات في Dockerfile")
